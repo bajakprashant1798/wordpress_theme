ca45806116cb74b35ae32ae17bbcfd137dac2919
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 18:14:22 +0530

    try to add WordPress coding standards

--- contant-aside.php
+++ contant-aside.php
@@ -1,5 +1,4 @@
-<h3>aside post: <?php the_title(); ?> </h3>
-       
-            <small> post on <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
-
-            <br><br>
+<article class="post post-aside">
+    <p class="mini-meta"><?php the_author(); ?> @ <?php the_time('F j,Y') ?></p>
+    <?php the_content(); ?>
+</article>
